20303a8ca63e1bfcd64896721be8fb2159b3e6a4
Gabriel <reyendis@ext.falabella.cl>
2024-06-25 14:37:57 -0300

    carroussel edit

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const carousel = document.querySelector('.techstack');
     const items = Array.from(document.querySelectorAll('.techstack-item'));
 
-    // Duplicar los elementos del carrusel para la ilusión de bucle infinito
-    items.forEach(item => {
-        const clone = item.cloneNode(true);
-        carousel.appendChild(clone);
-    });
+    while (true) {
+        // Duplicar los elementos del carrusel para la ilusión de bucle infinito
+        items.forEach(item => {
+            const clone = item.cloneNode(true);
+            carousel.appendChild(clone);
+        });
+    }
 });
